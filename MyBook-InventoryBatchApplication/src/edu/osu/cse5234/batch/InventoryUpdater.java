ce5f0af52e32805f2b4ad349fdc2e2bc94978337
Logan McPherson <mcpherson.154@osu.edu>
2018-11-14 18:51:29 -0500

    skeleton code

--- MyBook-InventoryBatchApplication/src/edu/osu/cse5234/batch/InventoryUpdater.java
+++ MyBook-InventoryBatchApplication/src/edu/osu/cse5234/batch/InventoryUpdater.java
@@ -1,5 +1,63 @@
 package edu.osu.cse5234.batch;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
 public class InventoryUpdater {
+	public static void main(String[] args) {
+
+		System.out.println("Starting Inventory Update ...");
+		try {
+			Connection conn = createConnection();
+			Collection<Integer> newOrderIds = getNewOrders(conn);
+			Map<Integer, Integer> orderedItems = getOrderedLineItems(newOrderIds, conn);
+			udpateInventory(orderedItems, conn);
+			udpateOrderStatus(newOrderIds, conn);
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static Connection createConnection() throws SQLException, ClassNotFoundException {
+		Class.forName("org.h2.Driver");
+		Connection conn = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
+		return conn;
+	}
+
+	private static Collection<Integer> getNewOrders(Connection conn) throws SQLException {
+		Collection<Integer> orderIds = new ArrayList<Integer>();
+		ResultSet rset = conn.createStatement().executeQuery(
+                     "select ID from CUSTOMER_ORDER where STATUS = 'New'");
+		while (rset.next()) {
+			orderIds.add(new Integer(rset.getInt("ID")));
+		}
+		return orderIds;
+	}
+
+	private static Map<Integer, Integer> getOrderedLineItems(Collection<Integer> newOrderIds,
+                Connection conn)  throws SQLException {
+		// TODO Auto-generated method stub
+		// This method returns a map of two integers. The first Integer is item ID, and 
+                 // the second is cumulative requested quantity across all new orders
+		return null;
+	}
+
+	private static void udpateInventory(Map<Integer, Integer> orderedItems, 
+                Connection conn) throws SQLException {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void udpateOrderStatus(Collection<Integer> newOrderIds, 
+                Connection conn) throws SQLException {
+		// TODO Auto-generated method stub
+
+	}
 
 }
